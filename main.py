16fe920e4abeaff9f8beeec637d3d5c09d5e1233
Kelly <kelly.applebee@studio.unibo.it>
2026-04-04 15:54:56 +0200

    feat: exercise 1

---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np, pandas as pd, matplotlib.pyplot as plt, os
+from urllib.request import urlretrieve
+
+def setup():
+    
+    DATA_URL = "https://github.com/datascienceunibo/dialab2024/raw/main/Regressione_Lineare/power.csv"
+
+    if not os.path.exists("power.csv"):
+        urlretrieve(DATA_URL, "power.csv")
+
+
+def main():
+    setup()
+    
+    data = pd.read_csv("power.csv", parse_dates=["date"], index_col="date")
+    
+    # print(data.tail(3))
+    
+    # print(data.index.month)
+
+    # print((data.index.month >= 6) & (data.index.month <= 8))
+    
+    # print(data.loc[(data.index.month >= 6) & (data.index.month <= 8)])
+    
+    data_summer = data.loc[data.index.month.isin([6, 7, 8])]
+    
+    # print(data_summer.head(3))
+    # print(data_summer.tail(3))
+    
+    # data_summer["temp"].plot.hist(bins=20)
+    # data_summer["demand"].plot.hist(bins=20)
+    
+    data_summer.plot.scatter("temp", "demand")
+    
+    plt.show()
+    
+    print(data["temp"].describe())
+    print(data["demand"].describe())
+
+
+if __name__ == "__main__":
+    main()
